dd07b56f9476ace61a63da911524dd81052f7957
Wenfeng(Kevin) Wu <wenfengwu92@gmail.com>
2021-12-18 14:34:46 -0800

    edit Movie Model02

--- popcorn-backend/src/main/java/com/popcornbackend/controllers/MovieController.java
+++ popcorn-backend/src/main/java/com/popcornbackend/controllers/MovieController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,8 +22,15 @@ public class MovieController {
     @Autowired
     MovieService movieService;
 
+    //get movie by its id
+//    @GetMapping("/{id}")
+//    public ResponseEntity<Movie> getOne(@PathVariable(value = "id") String id) {
+//        Movie oneMovie = movieService.findMovieById(id);
+//        return ResponseEntity.ok(oneMovie);
+//    }
+
     @GetMapping("/{id}")
-    public Movie getOne(@PathParam(value = "id") String id) {
+    public Movie getOne(@PathVariable(value = "id") String id) {
         return movieService.findMovieById(id);
     }
 
@@ -42,13 +50,39 @@ public class MovieController {
         return new ResponseEntity<>(resp, HttpStatus.OK);
     }
 
+    //get movies by genres
     @GetMapping("/categories/{genres}")
-    public List<Movie> getByGenres(@PathParam("genres") String genres){
-        return movieService.getMoviesByGenres(genres);
+    public List<Movie> getByGenres(@PathVariable("genres") String genres){
+        ArrayList<String> newGen = new ArrayList<String>();
+        newGen.add(genres);
+        return movieService.getMoviesByGenres(newGen);
     }
 
+    //get movies by language
     @GetMapping("/categories/{language}")
-    public List<Movie> getByLanguage(@PathParam("language") String lan){
-        return movieService.getMoviesByGenres(lan);
+    public List<Movie> getByLanguage(@PathVariable("language") String language){
+        return movieService.getMoviesByLanguage(language);
+    }
+
+    //get 12 top score movies
+    @GetMapping("/score")
+    public ResponseEntity<Map<String, Object>> getAllAndSortByScore(
+            @RequestParam(value = "size", defaultValue = "12") int size,
+            @RequestParam(value = "page", defaultValue = "0") int page
+    ) {
+        PageRequest request = PageRequest.of(page, size);
+        List<Movie> movies = movieService.getMoviesAndSortByScore(request);
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("movies", movies);
+        resp.put("currentPage", page);
+        resp.put("nextPage", page + 1);
+        resp.put("totalSize", movies.size());
+        return new ResponseEntity<>(resp, HttpStatus.OK);
+    }
+
+    //get movies by release year
+    @GetMapping("/year/{year}")
+    public List<Movie> getByYear(@PathVariable("year") int year){
+        return movieService.getMoviesByStartYear(year);
     }
 }
--- popcorn-backend/src/main/java/com/popcornbackend/repos/MovieRepo.java
+++ popcorn-backend/src/main/java/com/popcornbackend/repos/MovieRepo.java
@@ -4,6 +4,7 @@ import com.popcornbackend.models.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -22,14 +23,17 @@ public interface MovieRepo extends MongoRepository<Movie, String> {
 
     Page<Movie> findAll(Pageable pageable);
 
-    //find all movies which primary title contain the query and sort by score descending order
-    Page<Movie> findAllByPrimaryTitleOrderByScoreDesc(String query);
+    //find all by movie genres, but the variable is a list of string
+    List<Movie> findAllByGenres(List<String> genre);
 
-    List<Movie> findAllByGenresContaining(String genre);
-
+    //find all by language
     List<Movie> findAllByLanguage(String lan);
 
-    List<Movie> findAllByScoreGreaterThan(float score);
+    //find all and sort by score descending
+    Page<Movie> findAllByOrderByScoreDesc(Pageable pageable);
+
+    //find all by release year
+    List<Movie> findAllByStartYear(int year);
 
 
 }

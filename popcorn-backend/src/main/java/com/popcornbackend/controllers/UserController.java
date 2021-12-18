a428a8c7de6c9f39b530dcb14f47fcbf1651d2c3
William Li <infinks1990@gmail.com>
2021-12-18 12:02:19 -0800

    add a PutMapping on UserController

--- popcorn-backend/src/main/java/com/popcornbackend/controllers/UserController.java
+++ popcorn-backend/src/main/java/com/popcornbackend/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.popcornbackend.controllers;
 
 import com.popcornbackend.models.User;
 import com.popcornbackend.services.UserService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,13 @@ public class UserController {
             resp.put("error", "Email already Exists");
             return new ResponseEntity<>(resp, HttpStatus.FORBIDDEN);
         }
+
+//        if(!currentUser.getPassword().equals(user.getPassword())){
+//            Map<String, Object> resp = new HashMap<>();
+//            resp.put("error", "Password does not match");
+//            return new ResponseEntity<>(resp, HttpStatus.FORBIDDEN);
+//        }
+
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userService.createUser(user);
         return new ResponseEntity<>(currentUser, HttpStatus.OK);
@@ -45,4 +53,18 @@ public class UserController {
         resp.put("error", "Password mismatch or user not found");
         return new ResponseEntity<>(resp, HttpStatus.FORBIDDEN);
     }
+
+    @PutMapping("/edit/{user}")
+    public ResponseEntity<Object> editUser(@PathVariable User user){
+        User currentUser = userService.findUser(user.getEmail());
+        if (currentUser != null) {
+            Map<String, Object> resp = new HashMap<>();
+            resp.put("error", "");
+            return new ResponseEntity<>(resp, HttpStatus.FORBIDDEN);
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userService.editUser(user);
+        return new ResponseEntity<>(currentUser, HttpStatus.OK);
+    }
+
 }

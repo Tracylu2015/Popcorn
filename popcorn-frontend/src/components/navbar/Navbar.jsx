30fb69a43aa8ff6e3a06a990dd527ed14b97575e
William Li <infinks1990@gmail.com>
2021-12-19 13:58:27 -0800

    Change className again

--- popcorn-frontend/src/components/navbar/Navbar.jsx
+++ popcorn-frontend/src/components/navbar/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
                 <form>
                     <button className="btn btn-square btn-ghost">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" className="bg-neutral text-neutral-content"></path>
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" className="bg-neutral text-neutral-content"></path>
                         </svg>
                     </button>
                 </form>

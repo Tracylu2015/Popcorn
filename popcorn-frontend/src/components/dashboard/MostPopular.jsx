c08d2b37e9f79bbe17e16feec32bf9a00b74472c
Tracylu2015 <89538089+Tracylu2015@users.noreply.github.com>
2021-12-19 14:28:45 -0800

    watchStatus work

--- popcorn-frontend/src/components/dashboard/MostPopular.jsx
+++ popcorn-frontend/src/components/dashboard/MostPopular.jsx
@@ -13,33 +13,32 @@ const MostPopular = () => {
     }, [])
 
     const OnMovieStatusChanged = (newMovie) => {
-        const newMovies=[]
-        movies.forEach((m)=>{
-            m.id===newMovie.id? newMovies.push(newMovie): newMovies.push(m)})
+        const newMovies = []
+        movies.forEach((m) => {
+            m.id === newMovie.id ? newMovies.push(newMovie) : newMovies.push(m)
+        })
         setMovies(newMovies)
     }
 
     return (
         <div>
             <h3>Most Popular Movies</h3>
-            <div className="card text-center shadow-2xl" >
-            <ul style={{ display: "flex"}}>
-                {movies.map(m =>
-                    <Link to={`/movies/detail/${m.id}`} key={m.id} style={{textDecoration: "none"}}>
+            <div className="card text-center shadow-2xl">
+                <ul style={{ display: "flex" }}>
+                    {movies.map(m =>
                         <li key={m.id} style={{ listStyle: "none" }}>
-                            <img src={m.imageUrl} className="rounded-xl" />
+                            <Link to={`/movies/detail/${m.id}`} key={m.id} style={{ textDecoration: "none" }}><img src={m.imageUrl} className="rounded-xl" /></Link>
                             <div className="card-body card-actions ">
-                            <h2 className="card-title text-neutral-focus ">{m.primaryTitle}</h2>
-                            <p className="text-warning">Movie Score: {m.score}</p>
-                            <div className="justify-center card-actions">
-                            <WatchStatus movie={m} onChange={OnMovieStatusChanged}/>
-                            <Rateit movie={m}/>
-                            </div>
+                                <h2 className="card-title text-neutral-focus ">{m.primaryTitle}</h2>
+                                <p className="text-warning">Movie Score: {m.score}</p>
+                                <div className="justify-center card-actions">
+                                    <WatchStatus movie={m} onChange={OnMovieStatusChanged} />
+                                    <Rateit movie={m} />
+                                </div>
                             </div>
                         </li>
-                    </Link>
-                )}
-            </ul>
+                    )}
+                </ul>
             </div>
         </div>
     )

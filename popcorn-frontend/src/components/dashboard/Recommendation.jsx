4bf236b438f4e62217c5193c2a0fa16735bafcc6
Tracylu2015 <89538089+Tracylu2015@users.noreply.github.com>
2021-12-20 15:29:02 -0800

    add recommend movies to frontend

--- popcorn-frontend/src/components/dashboard/Recommendation.jsx
+++ popcorn-frontend/src/components/dashboard/Recommendation.jsx
@@ -4,7 +4,7 @@ import WatchStatus from '../WatchStatus'
 import Rating from 'react-rating'
 import pop_empty from "../../images/pop_blank.png"
 import pop_fill from "../../images/pop_fill.png"
-
+import { Container } from 'react-bootstrap'
 
 const Recommendation = () => {
     const [recMovies, setRecMovies] = useState([])
@@ -15,26 +15,26 @@ const Recommendation = () => {
             }).catch(err => console.log(err))
     }, [])
     return (
-
-        <div>
+        <Container>
             <h3>Recommendations</h3>
             <ul>
                 {recMovies.map(m => {
-                    <li key={m.id}>
-                        <img src={m.imageUrl} alt="movie poster" />
-                        <p>Title: {m.primaryTitle}</p>
-                        <p>Score: {m.score}</p>
-                        <WatchStatus />
-                        <Rating
-                            style={{ marginTop: "10px" }}
-                            readonly="true" stop="10" step="2" initialRating={m.score}
-                            emptySymbol={<img src={pop_empty} className="icon" style={{ width: "30px", height: "30px" }} />}
-                            fullSymbol={<img src={pop_fill} className="icon" style={{ width: "30px", height: "30px" }} />}
-                        />
-                    </li>
+                    return (
+                        <li key={m.id}>
+                            <img src={m.imageUrl} alt="movie poster" />
+                            <p>Title: {m.primaryTitle}</p>
+                            <p>Score: {m.score}</p>
+                            <WatchStatus movie={m}/>
+                            <Rating
+                                style={{ marginTop: "10px" }}
+                                readonly="true" stop="10" step="2" initialRating={m.score}
+                                emptySymbol={<img src={pop_empty} className="icon" style={{ width: "30px", height: "30px" }} />}
+                                fullSymbol={<img src={pop_fill} className="icon" style={{ width: "30px", height: "30px" }} />}
+                            />
+                        </li>)
                 })}
             </ul>
-        </div>
+        </Container>
     )
 }
 

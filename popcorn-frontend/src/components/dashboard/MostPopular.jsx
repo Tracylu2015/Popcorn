e268aae5be1e4cb12a1aef27fd110107827ff594
Tracylu2015 <89538089+Tracylu2015@users.noreply.github.com>
2021-12-20 23:46:59 -0800

    add Carousel

--- popcorn-frontend/src/components/dashboard/MostPopular.jsx
+++ popcorn-frontend/src/components/dashboard/MostPopular.jsx
@@ -30,31 +30,31 @@ const MostPopular = () => {
 
     const responsive = {
         desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 6,
-          slidesToSlide: 3 // optional, default to 1.
+            breakpoint: { max: 3000, min: 1024 },
+            items: 6,
+            slidesToSlide: 3 // optional, default to 1.
         },
         tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 3,
-          slidesToSlide: 2 // optional, default to 1.
+            breakpoint: { max: 1024, min: 464 },
+            items: 3,
+            slidesToSlide: 2 // optional, default to 1.
         },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1,
-          slidesToSlide: 1 // optional, default to 1.
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
         }
-      };
+    };
 
     return (
         <Container>
             <h3>Most Popular Movies</h3>
-            <Carousel 
-                   responsive={responsive}
-                   autoPlay={true}
-                   autoPlaySpeed={3000}
-                   infinite={true}
-                   >
+            <Carousel
+                responsive={responsive}
+                autoPlay={true}
+                autoPlaySpeed={3000}
+                infinite={true}
+            >
                 {movies.map(m => {
                     return (
                         <Col key={m.idString}>

db6323adb3b3c4fc7fd1f6c8ea42332d5e2940ae
William Li <infinks1990@gmail.com>
2021-12-21 16:28:52 -0800

    mYpOST UPDATE

--- popcorn-frontend/src/components/user/MyPost.jsx
+++ popcorn-frontend/src/components/user/MyPost.jsx
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Container, Card} from 'react-bootstrap'
+import { Card, Row, Col, Container, Button } from 'react-bootstrap'
 
 const MyPost = () => {
 
@@ -12,28 +12,58 @@ const MyPost = () => {
         axios.get(`http://localhost:8080/api/comment/user`)
             .then(res => setMyPost(res.data))
             .catch((error) => console.log(error))
-    },[])
+    }, [])
 
     return (
-        <Container style={{ marginLeft: "40px", marginTop: "20px" }}>
-            {<h3>My Post</h3>}
-            {myPost.map((comment, i) => {
-                return(
-                    <Card key={i}>
-                    <div>
-                        <Card.Header as="h5">Movie: {comment.movie.primaryTitle}</Card.Header>
-                        <p>{comment.created}</p>
-                    </div>
-                    <Card.Body>
-                        <Card.Title>{comment.totalLikes}</Card.Title>
-                        <Card.Text>
-                        {comment.post}
-                        </Card.Text>
-                    </Card.Body>
-                    </Card>
-                )
-            })}
+        // <Container style={{ marginLeft: "40px", marginTop: "20px" }}>
+        //     {<h3>My Post</h3>}
+        //     {myPost.map((comment, i) => {
+        //         return(
+        //             <Card key={i}>
+        //             <div>
+        //                 <Card.Header as="h5">Movie: {comment.movie.primaryTitle}</Card.Header>
+        //                 <p>{comment.created}</p>
+        //             </div>
+        //             <Card.Body>
+        //                 <Card.Title>{comment.totalLikes}</Card.Title>
+        //                 <Card.Text>
+        //                 {comment.post}
+        //                 </Card.Text>
+        //             </Card.Body>
+        //             </Card>
+        //         )
+        //     })}
+        // </Container>
+        <Container>
+            <h3>My Post</h3>
+            <Row>
+                {myPost.map((comment, i) => {
+                    return (
+                        <Col sm={8} md={"auto"} style={{ marginTop: "20px" }}>
+                            <Card>
+                                <Card.Header as="h5">
+                                    <Row>
+                                        <Col>
+                                            Movie: {comment.movie.primaryTitle}&nbsp;&nbsp;&nbsp;&nbsp;
+                                        </Col>
+                                        <Col sm={1} md={"auto"}>
+                                            <Button size="sm" >Delete</Button>
+                                        </Col>
+                                    </Row>
+                                    <p style={{fontSize: 14}}>{comment.created}</p>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                        {comment.post}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
+                })}
+            </Row>
         </Container>
+
     )
 }
 
